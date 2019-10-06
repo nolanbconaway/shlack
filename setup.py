003f629c208104adaa3acb2d3112f1bd38e74329
Nolan Conaway <nolanbconaway@gmail.com>
2019-10-06 16:33:49 -0400

    set up for auto pypi publish

--- setup.py
+++ setup.py
@@ -4,28 +4,6 @@ import os
 
 from setuptools import find_packages, setup
 
-MIT_LICENSE = """
-Copyright (c) 2019 Nolan Conaway
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 # use readme as long description
 README_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "readme.md")
 
@@ -51,7 +29,7 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     keywords=["slack", "cli"],
-    license=MIT_LICENSE,
+    license="MIT",
     packages=["shlack", "shlack.cli"],
     install_requires=["slacker", "click"],
     entry_points={"console_scripts": ["shlack = shlack.cli.__main__:cli"]},
