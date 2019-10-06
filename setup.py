0bd23f978759443152e6a984125818a386e85060
Nolan Conaway <nolanbconaway@gmail.com>
2019-10-06 16:01:22 -0400

    add setup py metadata

--- setup.py
+++ setup.py
@@ -2,9 +2,46 @@
 
 from setuptools import find_packages, setup
 
+MIT_LICENSE = """
+Copyright (c) 2019 Nolan Conaway
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 setup(
     name="shlack",
     version="0.1.1",
+    description="Yet another slack command line interface.",
+    author="Nolan Conaway",
+    author_email="nolanbconaway@gmail.com",
+    url="https://github.com/nolanbconaway/shlack",
+    classifiers=[
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords=["slack", "cli"],
+    license=MIT_LICENSE,
     packages=["shlack", "shlack.cli"],
     install_requires=["slacker", "click"],
     entry_points={"console_scripts": ["shlack = shlack.cli.__main__:cli"]},
